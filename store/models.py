061ab5e8d9fc813c8b75b4927a3e0193c2212639
Dipaloke <dipalokebiswas96@gmail.com>
2026-01-27 13:39:55 +0600

    Feat: One to many relation.

--- store/models.py
+++ store/models.py
@@ -1,12 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=225)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    Collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+class Item(models.Model):
+    quantity = models.PositiveSmallIntegerField()
+    order = models.ForeignKey('Order', on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    # cart = models.ForeignKey('Cart', on_delete=models.PROTECT)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS__CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS__CHOICES, default=PAYMENT_STATUS_PENDING),
+    # items = models.ForeignKey(Item, on_delete=models.CASCADE)
+    customer = models.ForeignKey('Customer', on_delete=models.PROTECT)
 
 
 class Customer(models.Model):
@@ -26,25 +55,33 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    # orders = models.ForeignKey(Order, on_delete=models.CASCADE)
 
 
-class Order(models.Model):
-    PAYMENT_STATUS_PENDING = 'P'
-    PAYMENT_STATUS_COMPLETE = 'C'
-    PAYMENT_STATUS_FAILED = 'F'
 
-    PAYMENT_STATUS__CHOICES = [
-        (PAYMENT_STATUS_PENDING, 'Pending'),
-        (PAYMENT_STATUS_COMPLETE, 'Complete'),
-        (PAYMENT_STATUS_FAILED, 'Failed'),
-    ]
-    placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS__CHOICES, default=PAYMENT_STATUS_PENDING),
 
 
 # 1 to 1 relation
 
+# class Address(models.Model):
+#     street = models.CharField(max_length=255)
+#     city = models.CharField(max_length=255)
+#     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
+# one to many relations
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+
+class Cart:
+    created_at = models.DateTimeField(auto_now_add=True)
+    # items = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
